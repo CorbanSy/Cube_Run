d20a4d831265f11c308bd47d03bd0dd86322b851
CorbanSy <corbansy10@gmail.com>
2021-06-21 22:06:53 -0700

    Added fog and more game details
    Added fog so you cant see all of the new obstacles

--- Unity/New Unity Project/Assets/PlayerMovement.cs
+++ Unity/New Unity Project/Assets/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-    public float forwardForce = 2000f;
+    public float forwardForce = 500f;
     public float sideways = 500f;//put f because it is a float
     // Update is called once per frame
     void FixedUpdate()//made it fixedupdate brcause we are using physics
@@ -11,11 +11,11 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(0, 0, 2000 * Time.deltaTime);
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sideways * Time.deltaTime, 0, 0);
+            rb.AddForce(sideways * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sideways * Time.deltaTime, 0, 0);
+            rb.AddForce(-sideways * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
         }
     }
 }
