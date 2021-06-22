2ec85c3f52909bc9a96d555b95904c28467911a6
CorbanSy <corbansy10@gmail.com>
2021-06-21 18:43:55 -0700

    Added Movements
    Made it so the "a" and "d" move the cube left and right 200f

--- Unity/New Unity Project/Assets/PlayerMovement.cs
+++ Unity/New Unity Project/Assets/PlayerMovement.cs
@@ -3,11 +3,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-
-
+    public float forwardForce = 2000f;
+    public float sideways = 500f;//put f because it is a float
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate()//made it fixedupdate brcause we are using physics
+    {//added a forward force
         rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideways * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideways * Time.deltaTime, 0, 0);
+        }
     }
 }
